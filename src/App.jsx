b820810fb887b37452a6014c768267c1107d1fc9
Igorth <igordiasth@gmail.com>
2024-03-13 06:00:58 -0400

    feat: add CSS Modules on Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
 import { Post } from './Post';
+import { Header } from './components/Header';
+import './styles.css';
 
 export function App() {
   return (
     <>
+      <Header />
       <Post author="Igor" content="Lorem" />
       <Post author="Laisa" content="Lorem" />
     </>
