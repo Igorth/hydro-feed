6e1f799f8cb9091e25141352bed6622bac3ddc45
Igorth <igordiasth@gmail.com>
2024-03-18 14:20:37 -0400

    feat: add props in the Post components

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,62 @@ import { Sidebar } from './components/Sidebar';
 import styles from './App.module.css';
 import './global.css';
 
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://github.com/Igorth.png',
+      name: 'Igor Dias',
+      role: 'Web developer',
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galera 👋' },
+      {
+        type: 'paragraph',
+        content:
+          'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀',
+      },
+      { type: 'link', content: 'jane.design/doctorcare' },
+    ],
+    publishedAt: new Date('2022-05-03 20:00:00'),
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'https://github.com/IgorTh.png',
+      name: 'Laisa Agueda',
+      role: 'Student',
+    },
+    content: [
+      { type: 'paragraph', content: 'Segundo conteúdo 👋' },
+      {
+        type: 'paragraph',
+        content:
+          'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀',
+      },
+      { type: 'link', content: 'jane.design/doctorcare' },
+    ],
+    publishedAt: new Date('2022-05-10 20:00:00'),
+  },
+];
+
 export function App() {
   return (
-    <>
-      <Header />
+    <div>
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
-          <Post author="Igor" content="Lorem" />
-          <Post author="Laisa" content="Lorem" />
+          {posts.map((post) => {
+            return (
+              <Post
+                author={post.author}
+                content={post.content}
+                publishedAt={post.publishedAt}
+              />
+            );
+          })}
         </main>
       </div>
-    </>
+    </div>
   );
 }
