275d5902bf33fe9a76011e2a0de041c14514cdae
Igorth <igordiasth@gmail.com>
2024-03-19 05:41:48 -0400

    feat: add key in the lists of the components

--- src/App.jsx
+++ src/App.jsx
@@ -47,12 +47,15 @@ const posts = [
 export function App() {
   return (
     <div>
+      <Header />
+
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
           {posts.map((post) => {
             return (
               <Post
+                key={post.id}
                 author={post.author}
                 content={post.content}
                 publishedAt={post.publishedAt}
